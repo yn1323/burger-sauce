781ed1cc714c57544521da0f0d01eb4464316d0c
y-natani <yn1323@gmail.com>
2024-01-06 00:09:52 +0900

    chore: color

--- lib/components/styles/button.dart
+++ lib/components/styles/button.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 ButtonStyle getCardButtonFormStyle(BuildContext context) {
   return ElevatedButton.styleFrom(
     padding: const EdgeInsets.symmetric(vertical: 15.0),
-    foregroundColor: Theme.of(context).colorScheme.onPrimary,
-    backgroundColor: Theme.of(context).colorScheme.primary,
+    // TODO: ColorSchemaをあとで考える
+    // foregroundColor: Theme.of(context).colorScheme.onPrimary,
+    // backgroundColor: Theme.of(context).colorScheme.primary,
+    // foregroundColor: Colors.white,
+    backgroundColor: Colors.white,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(5),
     ),
